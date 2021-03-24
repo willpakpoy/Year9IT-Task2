f7be3fa91d899225aded629bba8c60ddc73bc80f
Will Pak Poy <0521858@radford.act.edu.au>
2021-03-25 09:22:26 +1100

    finish question 2

--- 2-round/__main__.py
+++ 2-round/__main__.py
@@ -31,7 +31,7 @@ Entry(window, textvariable=total_rounds).grid(column=0, row=1)
 Label(window, text="Rounds Joe has won").grid(column=0, row=4) # Label for the text field.
 Entry(window, textvariable=joe_rounds).grid(column=0, row=5)
 
-Label(window, text="Rounds jane has won").grid(column=1, row=4) # Label for the text field.
+Label(window, text="Rounds Jane has won").grid(column=1, row=4) # Label for the text field.
 Entry(window, textvariable=jane_rounds).grid(column=1, row=5)
 
 Button(window, text="Submit", command=lambda : compute()).grid(column=0, row=7) # Clicking this button runs the 'compute' function.
@@ -45,14 +45,33 @@ def compute():
     total=total_rounds.get()
 
     if total <= 1:
-        win_status.set("Total number of rounds must be greater than zero")
+        win_status.set("Total rounds must be greater than zero.")
         return
     
     if joe > total or jane > total:
-        
+        win_status.set("Personal number of rounds is higher than total.")
+        return
+
+    if joe+jane > total:
+        win_status.set("Personal number of rounds is higher than total.")
+        return
+    
+    if total / 2 < joe:
+        win_status.set("Impossible, Joe has won more than half of the rounds.")
+        return
+    elif total / 2 < jane:
+        win_status.set("Jane has already won.")
+        return
+
+    else:
+        jane_win_by = total / 2 +1 - jane
+        win_status.set(f"Jane must win {int(jane_win_by)} more rounds to secure a win against Joe.")
+        return
 
 col_count, row_count = window.grid_size()
 for row in range(row_count):
     window.grid_rowconfigure(row, minsize=5)
 
 window.mainloop()
+
+# TODO: comments + pseudocode
