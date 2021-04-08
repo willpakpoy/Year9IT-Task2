fc5b2fd56466688dfa9bce79e9c7dd35e46c1117
Will Pak Poy <0521858@radford.act.edu.au>
2021-04-08 14:52:22 +1000

    ...push

--- 3-rock/__main__.py
+++ 3-rock/__main__.py
@@ -5,25 +5,65 @@ window = Tk()
 window.minsize('600', '300')
 window.title('3 - Rock')
 
+
 num_rounds = 3
 
-most_recent_user_choice = StringVar()
-
+user_chooses_text = StringVar()
+python_chooses_text = StringVar()
+winner_text = StringVar()
 
 Label(window, text=f"Number of rounds: {num_rounds}", anchor="w", justify=LEFT).grid(column=0, row=0) # Label for the text field.
 Label(window, text=f"Please make a choice.", anchor="w", justify=LEFT).grid(column=0, row=1) # Label for the text field.
 
-Button(window, text="Submit", command=lambda : compute()).grid(column=0, row=1)
+Button(window, text="Rock", command=lambda : make_choice("rock")).grid(column=1, row=2)
+Button(window, text="Paper", command=lambda : make_choice("paper")).grid(column=2, row=2)
+Button(window, text="Scissors", command=lambda : make_choice("scissors")).grid(column=3, row=2)
 
+Label(window, textvariable=user_chooses_text, anchor="w", justify=LEFT).grid(column=0, row=4) # Label for the text field.
+Label(window, textvariable=python_chooses_text, anchor="w", justify=LEFT).grid(column=0, row=5) # Label for the text field.
+Label(window, textvariable=winner_text, anchor="w", justify=LEFT).grid(column=0, row=6) # Label for the text field.
 
-Label(window, text="Rounds Joe has won").grid(column=0, row=4) # Label for the text field.
-Entry(window, textvariable=joe_rounds).grid(column=0, row=5)
+def calculate_python_choice():
+    number_choice = randint(0,99)
+    if number_choice < 33:
+        return "rock"
+    elif number_choice >= 33 and number_choice < 66:
+        return "paper"
+    else:
+        return "scissors"
 
-Label(window, text="Rounds jane has won").grid(column=1, row=4) # Label for the text field.
-Entry(window, textvariable=jane_rounds).grid(column=1, row=5)
+def declare_winner(winner):
+    winner_text.set(winner)
 
+def make_choice(user_choice):
+    python_choice = calculate_python_choice()
 
-Label(window, text="Possibility for Jane to win").grid(column=0, row=8) # Label for the text field.
-Label(window, textvariable=win_status).grid(column=0, row=9) # Label for the text field.
+    user_chooses_text.set(f"User chooses {user_choice}")
+    python_chooses_text.set(f"Python chooses {python_choice}")
+
+    if user_choice == python_choice:
+        declare_winner("draw")
+    
+    elif user_choice == "rock":
+        if python_choice == "scissors":
+            declare_winner("user")
+            
+        else:
+            declare_winner("python")
+
+    elif user_choice == "scissors":
+        if python_choice == "paper":
+            declare_winner("user")
+            
+        else:
+            declare_winner("python")
+
+    elif user_choice == "paper":
+        if python_choice == "scissors":
+            declare_winner("user")
+        else:
+            declare_winner("python")
+
+    
 
 window.mainloop()
